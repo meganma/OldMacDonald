cda9bbb917bec597ed0b57bd598fc050beeaa1a3
meganma <meganmasf@hotmail.com>
2018-10-24 10:29:36 -0700

    Update Cow.java

--- Cow.java
+++ Cow.java
@@ -1,4 +1,18 @@
-class Cow //implements Animal 
-{     
-     //your code here
+class cow implements Animal {     
+     private String myType;     
+     private String mySound;      
+  
+     public cow(String type, String sound)     {         
+         myType = type;         
+         mySound = sound; 
+
+     }     
+     public cow()     {         
+         myType = "unknown";         
+         mySound = "unknown";   
+    
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
+  
 }
