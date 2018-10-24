521bc73f5e9ec2c72904c67b0413ea9659dfcafc
meganma <meganmasf@hotmail.com>
2018-10-24 10:30:13 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,14 @@
-class NamedCow
-{
-
+class NamedCow extends cow{
+  private String myName;
+  private String mySound;
+  private String myType;
+  public NamedCow(String type, String name, String sound ){
+    
+    myName=name;
+    mySound=sound;
+    myType=type;
+  }
+    
+    public String getName(){return myName;}
+    
 }
